c8469a301136694acd3befb34448576efcf8a442
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-10 18:57:49 +0100

    skills file added

---
+++ src/components/services/skills.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from 'next/link';
+
+
+function Skills() {
+  return (
+    <>
+      <div className="">
+        <div className="w-full h-[25em] text-charcoal">
+          my skiiils
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Skills;
