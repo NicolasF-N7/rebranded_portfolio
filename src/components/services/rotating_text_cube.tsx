ab317e904781aa77d1bc1ad4f50fe8a504d04a4a
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-21 16:26:58 +0100

    with file

---
+++ src/components/services/rotating_text_cube.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState, useRef } from 'react';
+import { gsap } from 'gsap';
+import { StaggerVars } from 'gsap';
+
+interface RotatingTextCubeProps {
+  texts: string[];
+}
+
+const RotatingTextCube: React.FC<RotatingTextCubeProps> = ({ texts }) => {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  // BABY STEPS, Make it simple
+  useEffect(() => {
+    const textElements = document.querySelectorAll('.rotatingHeaderText') as NodeListOf<HTMLElement>;
+
+    let ctx = gsap.context(() => {
+      // Create GSAP timeline for each text element
+      const tl = gsap.timeline({ repeat: -1});
+    
+      tl.to(textElements, {
+        duration: 0.5,
+        rotationX: -90,
+        delay: 2,
+        opacity: 0,
+        ease: "power4.in",
+        transformOrigin: "50% 50% -30",
+        onComplete: () => {
+          setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+          textElements.forEach((textElement) => {
+            textElement.textContent = texts[currentTextIndex];
+          });
+        }
+      });
+      tl.to(textElements, {
+        duration: 0.5,
+        rotationX: 0,
+        opacity: 1,
+        transformOrigin: "50% 50% -30"
+      });
+    });
+    // End of useEffect
+    return () => {
+        ctx.revert();
+    };
+  }, [texts, currentTextIndex]);
+
+  return (
+    <div className="inline-block rotatingHeader">
+      <h1 className="rotatingHeaderText">{texts[currentTextIndex]}</h1>
+    </div>
+  );
+};
+
+export default RotatingTextCube;
