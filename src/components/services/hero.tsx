e8d1efdca4ff0bae9f7b499d5285b065e09d6404
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-21 16:23:39 +0100

    aniamtion OKAY

--- src/components/services/hero.tsx
+++ src/components/services/hero.tsx
@@ -5,14 +5,12 @@ function Hero() {
   return (
     <>
       <div
-        className="text-zomp relative heroElem w-full pt-20 pb-40 m-auto flex justify-center text-center flex-col items-center z-1"
-        style={{ maxWidth: "1200px" }}
-      >
-        <p className="text-xl mb-5">Je suis Nicolas Foin,</p>
+        className="text-zomp relative heroElem pt-20 pb-40 flex text-center flex-col items-center z-1"
+        style={{ maxWidth: "1200px" }}>
         
-        <h1 className="leading-[1.5] heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
+        <h1 className="leading-[1.5] heroTitle inline-block max-w-2xl lg:max-w-4xl relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
           Je crée des <span className="heroShiny1 text-charcoal">sites web</span> pour votre{" "}
-          <span className="heroShiny2 text-charcoal"><RotatingTextCube texts={["entreprise", "commerce", 'startup']}/></span>.
+          <span className="heroShiny2 text-charcoal"><RotatingTextCube texts={["entreprise", "commerce", 'startup']}/></span>
           
 
 
