bf5f4eafbd56a1b5a9dcf8c31f2eb20698e03190
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-22 18:38:47 +0100

    Added profile picture

--- src/components/services/hero.tsx
+++ src/components/services/hero.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Image from "next/image";
 import RotatingTextCube from "@/components/services/rotating_text_cube";
+import profilePicture from '/public/photos_nicolas/transparent-closecut-1080-light.png'
 
 function Hero() {
   return (
     <>
       <div
-        className="text-zomp relative heroElem pt-20 pb-40 flex text-center flex-col items-center z-1"
+        className="text-zomp relative heroElem pt-20 pb-40 flex flex-col text-center flex-col items-center z-1"
         style={{ maxWidth: "1200px" }}>
         
         <h1 className="leading-[1.5] heroTitle inline-block max-w-2xl lg:max-w-4xl relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
@@ -30,11 +32,11 @@ function Hero() {
             style={{ animationDelay: "0.5s" }}
             src="/hero-icons/js.svg"
           />
-          <img
+          {/* <img
             className="sqD w-[20%] opacity-40 bottom-[-130px] sm:bottom-[-135%] right-[30%]"
             style={{ animationDelay: "1.0s" }}
             src="/hero-icons/store.svg"
-          />
+          /> */}
           <img
             className="sqD bottom-[-55%] right-[-5%] sm:bottom-[-75%] sm:right-[+5%] w-[20%] opacity-40"
             style={{ animationDelay: "0.3s" }}
@@ -47,6 +49,15 @@ function Hero() {
           />
           
         </h1>
+
+        {/* Profile picture */}
+        <div className="flex justify-center md:mb-[10em]">
+          <Image
+              className=" sqD w-[50%] md:w-[35%] rounded-md"
+              src={profilePicture}
+              alt="Nicolas Profile picture"/>
+        </div>
+
       </div>
     </>
   );
