dc4e4640a7437384c9f3920f8a6fca7b13b27ebc
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-23 12:18:56 +0100

    Profil pict added

--- src/components/services/hero.tsx
+++ src/components/services/hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
   return (
     <>
       <div
-        className="text-zomp relative heroElem pt-20 pb-40 flex flex-col text-center flex-col items-center z-1"
+        className="text-zomp relative heroElem pt-20 pb-20 sm:pb-40 flex flex-col text-center flex-col items-center z-1"
         style={{ maxWidth: "1200px" }}>
         
         <h1 className="leading-[1.5] heroTitle inline-block max-w-2xl lg:max-w-4xl relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
