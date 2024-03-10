95e179e835969edb7724e4009c5e2cebc2e8aaa0
Nico <nicolasf-n7@users.noreply.github.com>
2024-03-10 13:35:30 +0100

    Revert "Merge branch 'vercel_deploy'"
    This reverts commit 7c608c2689177306070644a4f03ffa68f8e01456, reversing
    changes made to 3a876b49bbcd2ab34afbcaf28a748a9c745a7898.

--- src/components/services/rotating_text_cube.tsx
+++ src/components/services/rotating_text_cube.tsx
@@ -12,59 +12,31 @@ const RotatingTextCube: React.FC<RotatingTextCubeProps> = ({ texts }) => {
   useEffect(() => {
     const textElements = document.querySelectorAll('.rotatingHeaderText') as NodeListOf<HTMLElement>;
 
-     //make a clone of the h1 tag
-		let nextText = document.querySelector(".rotatingNextText");
-		let nextTextChars = nextText.querySelectorAll(".chars")
-		
-    let currentText = document.querySelector(".rotatingCurrentText");
-		let currentTextChars = currentText.querySelectorAll(".chars")
-    
     let ctx = gsap.context(() => {
       // Create GSAP timeline for each text element
       const tl = gsap.timeline({ repeat: -1});
-
-      // Original animation
-      let duration = 0.4
-      let stagger = {each:0.02, ease:"power2", from:"start"}
-
-      //initial position of clones
-      gsap.set(nextText, {
-        rotationX:-90, 
-        opacity:0, 
-        transformOrigin:"50% 50% -50"
-      })
-      
-      //TAKE BACK HERE, ANALYSE ROATATING
-      //build animation
-      tl.to(currentTextChars, {
-        duration:duration, 
-        rotationX:90,
+    
+      tl.to(textElements, {
+        duration: 0.5,
+        rotationX: -90,
         delay: 2,
-        transformOrigin: "50% 50% -50",
-        stagger:{each:0.02, ease:"power2", from:"start"}
+        opacity: 0,
+        ease: "power4.in",
+        transformOrigin: "50% 50% -30",
+        onComplete: () => {
+          setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+          textElements.forEach((textElement) => {
+            textElement.textContent = texts[currentTextIndex];
+          });
+        }
       });
-      tl.to(currentTextChars, {
-        duration:duration, 
-        opacity:0,
-        delay: 2,
-        stagger:{each:0.02, ease:"power2", from:"start"}, 
-        ease:"power4.in"
-      }, 0)
-      
-      tl.to(nextTextChars, {
-        duration:0.05, 
-        opacity:1,
-        delay: 2,
-        stagger:{each:0.02, ease:"power2", from:"start"}
-      }, 0.001)
-      tl.to(nextTextChars, {
-        duration:duration, 
-        rotationX:0,
-        delay: 2,
-        stagger:{each:0.02, ease:"power2", from:"start"}
-      }, 0)
-    });//End of gsap.context
-
+      tl.to(textElements, {
+        duration: 0.5,
+        rotationX: 0,
+        opacity: 1,
+        transformOrigin: "50% 50% -30"
+      });
+    });
     // End of useEffect
     return () => {
         ctx.revert();
@@ -73,19 +45,7 @@ const RotatingTextCube: React.FC<RotatingTextCubeProps> = ({ texts }) => {
 
   return (
     <div className="inline-block rotatingHeader">
-      {/* Next text, to be coming. */}
-      <h1 className="text-left rotatingNextText" style={{ width: `${textsMaxCharLength}ch` }}>
-        {texts[(currentTextIndex - 1 + texts.length) % texts.length].split("").map((char, index) => (
-          <span className="chars" key={index}>{char}</span>
-        ))}
-      </h1>
-      
-      {/* Current text */}
-      <h1 className="text-left rotatingCurrentText" style={{ width: `${textsMaxCharLength}ch` }}>
-        {texts[currentTextIndex].split("").map((char, index) => (
-          <span className="chars" key={index}>{char}</span>
-        ))}
-      </h1>
+      <h1 className="text-left rotatingHeaderText" style={{ width: `${textsMaxCharLength}ch` }}>{texts[currentTextIndex]}</h1>
     </div>
   );
 };
